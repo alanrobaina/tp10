b01bcc4adaf64cb0e750a2b6bb5e4ad563c02803
alanrobaina <alanlakd@outlook.com>
2023-05-28 20:00:02 -0300

    trabajo practico 10

---
+++ tp10.js
@@ -0,0 +1,15 @@
+// se suma
+let numero1, numero2, vsuma, vvsuma; 
+numero1 = 123
+numero2 = 1234
+numero3 = 456
+vsuma = numero1 + numero2;
+vvsuma = vsuma + numero3;
+document.write("la suma es: " + vsuma);
+document.write(" la suma de la suma es: " + vvsuma);
+
+// // concatenar
+let nombre = "Alan"
+let apellido = "Robaina"
+let union = nombre + apellido
+console.log(nombre + " " + apellido)
